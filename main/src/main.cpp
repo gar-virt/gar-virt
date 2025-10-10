c5e963ad2b3a8c916115069e2a015fa21cc23b46
Steffen André Langnes <steffenl.sw@gmail.com>
2025-10-10 23:32:51 +0900

    More work

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,29 +1,49 @@
+#include "ping/v1/services.grpc.pb.h"
+#include "runner/v1/services.grpc.pb.h"
 #include <utility/env.hpp>
 
 #include <boost/json.hpp>
 #include <curl/curl.h>
 #include <grpc++/grpc++.h>
 
+#include <cstddef>
 #include <cstring>
 #include <expected>
+#include <optional>
 #include <print>
 #include <string>
 #include <string_view>
+#include <thread>
 #include <vector>
 
 namespace gitea {
 
+enum class error_code {
+  curl_error,
+  encode_payload_failed,
+  decode_payload_failed,
+};
+
 namespace {
-auto write_fn(void const *buffer, size_t size, size_t count,
-              std::string *output) -> size_t {
-  output->append(static_cast<char const *>(buffer), size * count);
+auto write_header_fn(char const *buffer, size_t size, size_t count,
+                     std::string *output) -> size_t {
+  output->append(buffer, size * count);
+  return size * count;
+}
+
+auto write_body_fn(void const *buffer, size_t size, size_t count,
+                   std::vector<std::byte> *output) -> size_t {
+  auto const old_size{output->size()};
+  output->resize(output->size() + size * count);
+  auto *output_offset{output->data() + old_size};
+  std::memcpy(output_offset, buffer, size * count);
   return size * count;
 }
 } // namespace
 
 struct http_response {
-  bool ok{};
-  boost::json::value body;
+  int status{};
+  std::vector<std::byte> body;
 };
 
 class http_header_source {
@@ -46,16 +66,16 @@ public:
   }
 
   auto post(std::string const &path,
-            boost::json::value const &payload_json) const -> http_response {
-    auto payload{boost::json::serialize(payload_json)};
+            std::vector<std::byte> const &payload) const
+      -> std::expected<http_response, error_code> {
     auto const url{m_base_url + path};
 
     std::string response_headers;
-    std::string response_body;
+    std::vector<std::byte> response_body;
 
     curl_slist *headers{};
-    headers = curl_slist_append(headers, "Accept: application/json");
-    headers = curl_slist_append(headers, "Content-Type: application/json");
+    headers = curl_slist_append(headers, "Accept: application/proto");
+    headers = curl_slist_append(headers, "Content-Type: application/proto");
 
     m_header_source->set_headers(
         [&](std::string const &name, std::string const &value) {
@@ -73,14 +93,14 @@ public:
       curl_easy_setopt(curl, CURLOPT_HEADER, 0);
       curl_easy_setopt(curl, CURLOPT_POST, 1);
       curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-      curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, write_fn);
-      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_fn);
+      curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, write_header_fn);
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_body_fn);
       curl_easy_setopt(curl, CURLOPT_HEADERDATA, &response_headers);
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_body);
       curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5);
       curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
       curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.c_str());
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.data());
       curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payload.size());
       curl_code = curl_easy_perform(curl);
       if (curl_code == CURLE_OK) {
@@ -92,17 +112,10 @@ public:
     curl_slist_free_all(headers);
 
     if (!curl || curl_code != CURLE_OK) {
-      return http_response{.ok = false};
+      return std::unexpected{error_code::curl_error};
     }
 
-    try {
-      return http_response{.ok = true,
-                           .body = boost::json::parse(response_body)};
-    } catch (std::exception const &) {
-      return http_response{.ok = false};
-    }
-
-    return http_response{.ok = false};
+    return http_response{.body = response_body};
   }
 
 private:
@@ -110,198 +123,68 @@ private:
   std::shared_ptr<http_header_source> m_header_source;
 };
 
-struct service_error {};
+template <typename T>
+auto encode_payload(T const &msg)
+    -> std::expected<std::vector<std::byte>, error_code> {
+  auto const byte_size{msg.ByteSizeLong()};
+  std::vector<std::byte> data;
+  data.resize(static_cast<std::size_t>(byte_size));
+  if (!msg.SerializeToArray(data.data(), byte_size)) {
+    return std::unexpected{error_code::encode_payload_failed};
+  }
+  return data;
+}
+
+template <typename T>
+auto decode_payload(std::vector<std::byte> const &payload)
+    -> std::expected<T, error_code> {
+  T msg;
+  if (!msg.ParseFromArray(payload.data(), static_cast<int>(payload.size()))) {
+    return std::unexpected{error_code::decode_payload_failed};
+  }
+  return msg;
+}
+
+template <typename Request, typename Response>
+auto send_post_request(http_client const &client, std::string const &path,
+                       Request const &req)
+    -> std::expected<Response, error_code> {
+  return encode_payload(req)
+      .and_then([&](auto payload) { return client.post(path, payload); })
+      .and_then([](auto res) { return decode_payload<Response>(res.body); });
+}
 
 namespace ping {
 
-struct ping_request {
-  std::string data;
-
-  auto to_json() -> boost::json::value {
-    return boost::json::value{{"data", data}};
-  }
-};
-
-struct ping_response {
-  std::string data;
-
-  static auto from_json(boost::json::value const &v)
-      -> std::expected<ping_response, service_error> {
-    try {
-      auto const &o{v.as_object()};
-      return ping_response{.data = std::string{o.at("data").as_string()}};
-    } catch (std::exception const &) {
-      return std::unexpected{service_error{}};
-    }
-  }
-};
-
-auto ping(http_client const &client, ping_request req)
-    -> std::expected<ping_response, service_error> {
-  try {
-    auto res{client.post("/ping.v1.PingService/Ping", req.to_json())};
-    return ping_response::from_json(res.body);
-  } catch (std::exception const &) {
-    return std::unexpected{service_error{}};
-  }
-
-  return std::unexpected{service_error{}};
+auto ping(http_client const &client, ::ping::v1::PingRequest req)
+    -> std::expected<::ping::v1::PingResponse, error_code> {
+  return send_post_request<::ping::v1::PingRequest, ::ping::v1::PingResponse>(
+      client, "/ping.v1.PingService/Ping", req);
 }
 
 } // namespace ping
 
 namespace runner {
 
-enum class runner_status {
-  unspecified = 0,
-  idle = 1,
-  active = 2,
-  offline = 3,
-};
-
-struct runner {
-  std::int64_t id{};
-  std::string uuid;
-  std::string token;
-  std::string name;
-  runner_status status{};
-  std::string version;
-  std::vector<std::string> labels;
-  bool ephemeral{};
-
-  static auto from_json(boost::json::value const &v) -> runner {
-    auto const &o{v.as_object()};
-    runner r;
-    if (o.contains("id")) {
-      r.id = std::stoll(std::string{o.at("id").as_string()});
-    }
-    if (o.contains("uuid")) {
-      r.uuid = std::string{o.at("uuid").as_string()};
-    }
-    if (o.contains("token")) {
-      r.token = std::string{o.at("token").as_string()};
-    }
-    if (o.contains("name")) {
-      r.name = std::string{o.at("name").as_string()};
-    }
-    if (o.contains("status")) {
-      r.status = static_cast<runner_status>(o.at("status").as_int64());
-    }
-    if (o.contains("version")) {
-      r.version = std::string{o.at("version").as_string()};
-    }
-    if (o.contains("labels")) {
-      for (auto const &label : o.at("labels").as_array()) {
-        r.labels.emplace_back(label.as_string());
-      }
-    }
-    if (o.contains("ephemeral")) {
-      r.ephemeral = o.at("ephemeral").as_bool();
-    }
-    return r;
-  }
-};
-
-struct task {};
-
-struct declare_request {
-  std::string version;
-  std::vector<std::string> labels;
-
-  auto to_json() -> boost::json::value {
-    return boost::json::object{{"version", version},
-                               {"labels", boost::json::value_from(labels)}};
-  }
-};
-
-struct declare_response {
-  struct runner runner;
-
-  static auto from_json(boost::json::value const &v)
-      -> std::expected<declare_response, service_error> {
-    return declare_response{.runner =
-                                runner::from_json(v.at("runner").as_object())};
-  }
-};
-
-struct register_request {
-  std::string name;
-  std::string token;
-  std::string version;
-  std::vector<std::string> labels;
-  bool ephemeral{};
-
-  auto to_json() -> boost::json::value {
-    return boost::json::object{{"token", token},
-                               {"name", name},
-                               {"version", version},
-                               {"labels", boost::json::value_from(labels)},
-                               {"ephemeral", ephemeral}};
-  }
-};
-
-struct register_response {
-  struct runner runner;
-
-  static auto from_json(boost::json::value const &v) -> register_response {
-    return register_response{.runner =
-                                 runner::from_json(v.at("runner").as_object())};
-  }
-};
-
-struct fetch_task_request {
-  std::int64_t tasks_version{};
-
-  auto to_json() -> boost::json::value {
-    return boost::json::object{{"tasks_version", tasks_version}};
-  }
-};
-
-struct fetch_task_response {
-  struct task task;
-  std::int64_t tasks_version{};
-
-  static auto from_json(boost::json::value const &v)
-      -> std::expected<fetch_task_response, service_error> {
-    return fetch_task_response{.tasks_version = std::stoll(std::string{
-                                   v.at("tasksVersion").as_string()})};
-  }
-};
-
-auto register_(http_client const &client, register_request req)
-    -> std::expected<register_response, service_error> {
-  try {
-    auto res{client.post("/runner.v1.RunnerService/Register", req.to_json())};
-    return register_response::from_json(res.body);
-  } catch (std::exception const &) {
-    return std::unexpected{service_error{}};
-  }
-
-  return std::unexpected{service_error{}};
+auto register_(http_client const &client, ::runner::v1::RegisterRequest req)
+    -> std::expected<::runner::v1::RegisterResponse, error_code> {
+  return send_post_request<::runner::v1::RegisterRequest,
+                           ::runner::v1::RegisterResponse>(
+      client, "/runner.v1.RunnerService/Register", req);
 }
 
-auto declare(http_client const &client, declare_request req)
-    -> std::expected<declare_response, service_error> {
-  try {
-    auto res{client.post("/runner.v1.RunnerService/Declare", req.to_json())};
-    return declare_response::from_json(res.body);
-  } catch (std::exception const &) {
-    return std::unexpected{service_error{}};
-  }
-
-  return std::unexpected{service_error{}};
+auto declare(http_client const &client, ::runner::v1::DeclareRequest req)
+    -> std::expected<::runner::v1::DeclareResponse, error_code> {
+  return send_post_request<::runner::v1::DeclareRequest,
+                           ::runner::v1::DeclareResponse>(
+      client, "/runner.v1.RunnerService/Declare", req);
 }
 
-auto fetch_task(http_client const &client, fetch_task_request req)
-    -> std::expected<fetch_task_response, service_error> {
-  try {
-    auto res{client.post("/runner.v1.RunnerService/FetchTask", req.to_json())};
-    return fetch_task_response::from_json(res.body);
-  } catch (std::exception const &) {
-    return std::unexpected{service_error{}};
-  }
-
-  return std::unexpected{service_error{}};
+auto fetch_task(http_client const &client, ::runner::v1::FetchTaskRequest req)
+    -> std::expected<::runner::v1::FetchTaskResponse, error_code> {
+  return send_post_request<::runner::v1::FetchTaskRequest,
+                           ::runner::v1::FetchTaskResponse>(
+      client, "/runner.v1.RunnerService/FetchTask", req);
 }
 
 } // namespace runner
@@ -349,8 +232,10 @@ struct runner_options {
   std::vector<std::string> runner_labels;
   std::optional<std::string> runner_uuid;
   std::optional<std::string> runner_token;
+  std::optional<bool> ephemeral{};
 
   static runner_options from_env() {
+    auto const ephemeral_string{utility::getenv("GITEA_RUNNER_EPHEMERAL")};
     runner_options o{
         .instance{utility::getenv("GITEA_INSTANCE_URL")},
         .runner_name{utility::getenv("GITEA_RUNNER_NAME")},
@@ -358,6 +243,9 @@ struct runner_options {
             utility::getenv("GITEA_RUNNER_REGISTRATION_TOKEN")},
         .runner_uuid{utility::getenv("GITEA_RUNNER_UUID")},
         .runner_token{utility::getenv("GITEA_RUNNER_TOKEN")},
+        .ephemeral{ephemeral_string ? std::make_optional(*ephemeral_string ==
+                                                         std::string{"1"})
+                                    : std::make_optional<bool>()},
     };
     if (auto const labels{utility::getenv("GITEA_RUNNER_LABELS")}) {
       o.runner_labels = parse_runner_labels_env_var(*labels);
@@ -371,7 +259,6 @@ struct runner_options {
 constexpr auto runner_version{std::string_view{"v0.1.0"}};
 
 auto cmd_register() -> int {
-  // GITEA_RUNNER_EPHEMERAL
   // GITEA_RUNNER_REGISTRATION_TOKEN_FILE
 
   auto const options{gitea::runner_options::from_env()};
@@ -399,28 +286,32 @@ auto cmd_register() -> int {
   auto header_source{std::make_shared<gitea::header_source>()};
   gitea::http_client client{*options.instance, header_source};
 
-  auto ping_result{gitea::ping::ping(
-      client, gitea::ping::ping_request{.data = *options.runner_name})};
-  if (!ping_result) {
+  auto ping_request{::ping::v1::PingRequest{}};
+  ping_request.set_data(*options.runner_name);
+  auto ping_response{gitea::ping::ping(client, ping_request)};
+  if (!ping_response) {
     std::println(std::cerr, "Error");
     return 1;
   }
 
-  auto reqister_request{gitea::runner::register_request{
-      .name = *options.runner_name,
-      .token = *options.runner_registration_token,
-      .version = std::string{runner_version},
-      .labels = options.runner_labels,
-      .ephemeral = true,
-  }};
-  auto register_result{gitea::runner::register_(client, reqister_request)};
-  if (!register_result) {
+  auto reqister_request{::runner::v1::RegisterRequest{}};
+  reqister_request.set_name(*options.runner_name);
+  reqister_request.set_token(*options.runner_registration_token);
+  reqister_request.set_version(std::string{runner_version});
+  for (auto &label : options.runner_labels) {
+    reqister_request.add_labels(std::move(label));
+  }
+  if (options.ephemeral) {
+    reqister_request.set_ephemeral(*options.ephemeral);
+  }
+  auto register_response{gitea::runner::register_(client, reqister_request)};
+  if (!register_response) {
     std::println(std::cerr, "Error");
     return 1;
   }
 
-  std::println("uuid={}", register_result->runner.uuid);
-  std::println("token={}", register_result->runner.token);
+  std::println("uuid={}", register_response->runner().uuid());
+  std::println("token={}", register_response->runner().token());
 
   return 0;
 }
@@ -459,19 +350,36 @@ auto cmd_daemon() -> int {
   header_source->set_uuid(*options.runner_uuid);
   header_source->set_token(*options.runner_token);
 
-  auto declare_request{gitea::runner::declare_request{
-      .version = std::string{runner_version}, .labels = options.runner_labels}};
-  auto declare_result{gitea::runner::declare(client, declare_request)};
-  if (!declare_result) {
+  auto declare_request{::runner::v1::DeclareRequest{}};
+  declare_request.set_version(std::string{runner_version});
+  for (auto &label : options.runner_labels) {
+    declare_request.add_labels(std::move(label));
+  }
+  auto declare_response{gitea::runner::declare(client, declare_request)};
+  if (!declare_response) {
     std::println(std::cerr, "Error");
     return 1;
   }
 
-  auto fetch_task_request{gitea::runner::fetch_task_request{}};
-  auto fetch_task_result{gitea::runner::fetch_task(client, fetch_task_request)};
-  if (!fetch_task_result) {
-    std::println(std::cerr, "Error");
-    return 1;
+  std::int64_t last_task_version{};
+  for (int i{}; i < 30; ++i) {
+    auto fetch_task_request{::runner::v1::FetchTaskRequest{}};
+    fetch_task_request.set_tasks_version(last_task_version);
+    auto fetch_task_response{
+        gitea::runner::fetch_task(client, fetch_task_request)};
+    if (!fetch_task_response) {
+      std::println(std::cerr, "Error");
+      return 1;
+    }
+    std::println("tasks_version={}", fetch_task_response->tasks_version());
+    //last_task_version = fetch_task_response->tasks_version(); // causes multiple tasks to not be given?
+
+    if (fetch_task_response->has_task()) {
+      std::println("task ID={}", fetch_task_response->task().id());
+    }
+
+    using namespace std::literals;
+    std::this_thread::sleep_for(1s);
   }
 
   return 0;

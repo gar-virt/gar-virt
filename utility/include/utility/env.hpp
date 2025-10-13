1f1b44c4f522fa3f9ddcc3bdc1f323851c77e0ca
Steffen André Langnes <steffenl.sw@gmail.com>
2025-10-14 02:34:31 +0900

    Add unsetenv and fixes for setenv/getenv

--- utility/include/utility/env.hpp
+++ utility/include/utility/env.hpp
@@ -7,5 +7,6 @@ namespace ls_gitea_runner::utility {
 
 std::optional<std::string> getenv(const std::string& name);
 void setenv(const std::string& name, const std::string& value);
+void unsetenv(const std::string& name);
 
 } // namespace ls_gitea_runner::utility
--- utility/src/env.cpp
+++ utility/src/env.cpp
@@ -1,27 +1,51 @@
 #include "utility/env.hpp"
-#include "utility/string.hpp"
 
 #include <cstdlib>
 
 #ifdef _WIN32
+    #include "utility/string.hpp"
+
+    #include <vector>
+
     #include <windows.h>
 #endif
 
 namespace ls_gitea_runner::utility {
 
-auto getenv(const std::string& name) -> std::optional<std::string> {
-    if (auto value = std::getenv(name.c_str())) {
-        return value;
-    } else {
-        return std::nullopt;
+std::optional<std::string> getenv(const std::string& name) {
+#ifdef _WIN32
+    const auto name_w{widen_string(name)};
+    const auto needed{::GetEnvironmentVariableW(name_w.c_str(), nullptr, 0)};
+    if (needed > 0) {
+        std::vector<wchar_t> value_w;
+        value_w.resize(needed);
+        const auto returned{::GetEnvironmentVariableW(name_w.c_str(), value_w.data(), needed)};
+        return narrow_string(value_w.data(), value_w.size() - 1);
     }
+#else
+    if (auto value{std::getenv(name.c_str())}) {
+        return value;
+    }
+#endif
+    return std::nullopt;
 }
 
-auto setenv(const std::string& name, const std::string& value) -> void {
+void setenv(const std::string& name, const std::string& value) {
 #ifdef _WIN32
-    SetEnvironmentVariableW(widen(name).c_str(), widen(value).c_str());
+    const auto name_w{widen_string(name)};
+    const auto value_w{widen_string(value)};
+    ::SetEnvironmentVariableW(name_w.c_str(), value_w.c_str());
 #else
     ::setenv(name.c_str(), value.c_str(), 1);
+#endif
+}
+
+void unsetenv(const std::string& name) {
+#ifdef _WIN32
+    const auto& wname{widen(name)};
+    SetEnvironmentVariableW(wname.c_str(), nullptr);
+#else
+    ::unsetenv(name.c_str());
 #endif
 }
 

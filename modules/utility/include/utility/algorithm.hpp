fa9b4913f5b0d7f8c1f9a0cc95fdf40c5ab20aff
Steffen André Langnes <steffenl.sw@gmail.com>
2026-05-25 15:14:41 +0900

    Fixes for safe_cast_int()

--- modules/utility/include/utility/algorithm.hpp
+++ modules/utility/include/utility/algorithm.hpp
@@ -1,33 +1,66 @@
 #pragma once
 
-#include <cstdint>
 #include <limits>
 #include <stdexcept>
 #include <type_traits>
 
 namespace ls_gitea_runner::utility {
 
-// Convert unsigned integral value to (un)signed
-template <typename R, typename T>
-typename std::enable_if<(std::is_signed<R>::value && !std::is_signed<T>::value) ||
-                            (std::is_signed<R>::value == std::is_signed<T>::value),
-                        R>::type
+/// Safely convert integral value from one type to another type where both are
+/// either signed or unsigned.
+template<typename R, typename T>
+typename std::enable_if<
+    std::is_signed<R>::value == std::is_signed<T>::value &&
+        std::is_integral<T>::value && std::is_integral<R>::value &&
+        !std::is_same<T, bool>::value && !std::is_same<R, bool>::value,
+    R>::type
 safe_cast_int(T value) {
-    using r_limits = std::numeric_limits<R>;
-    if (static_cast<uintmax_t>(value) > static_cast<uintmax_t>(r_limits::max())) {
+    using wider_t =
+        typename std::conditional<sizeof(T) >= sizeof(R), T, R>::type;
+    if (static_cast<wider_t>(value) <
+        static_cast<wider_t>(std::numeric_limits<R>::min())) {
+        throw std::invalid_argument{"value < result min"};
+    }
+    if (static_cast<wider_t>(value) >
+        static_cast<wider_t>(std::numeric_limits<R>::max())) {
         throw std::invalid_argument{"value > result max"};
     }
     return static_cast<R>(value);
 }
 
-// Convert signed integral value to unsigned
-template <typename R, typename T>
-typename std::enable_if<!std::is_signed<R>::value && std::is_signed<T>::value, R>::type safe_cast_int(T value) {
-    using r_limits = std::numeric_limits<R>;
+/// Safely convert integral value from signed type to unsigned type.
+template<typename R, typename T>
+typename std::enable_if<
+    !std::is_signed<R>::value && std::is_signed<T>::value &&
+        std::is_integral<T>::value && std::is_integral<R>::value &&
+        !std::is_same<T, bool>::value && !std::is_same<R, bool>::value,
+    R>::type
+safe_cast_int(T value) {
+    using wider_unsigned_t = typename std::conditional<
+        sizeof(T) >= sizeof(R), typename std::make_unsigned<T>::type, R>::type;
     if (value < 0) {
         throw std::invalid_argument{"value < 0"};
     }
-    if (static_cast<uintmax_t>(value) > static_cast<uintmax_t>(r_limits::max())) {
+    if (static_cast<wider_unsigned_t>(value) >
+        static_cast<wider_unsigned_t>(std::numeric_limits<R>::max())) {
+        throw std::invalid_argument{"value > result max"};
+    }
+    return static_cast<R>(value);
+}
+
+/// Safely convert integral value from unsigned type to signed type.
+template<typename R, typename T>
+typename std::enable_if<
+    std::is_signed<R>::value && !std::is_signed<T>::value &&
+        std::is_integral<T>::value && std::is_integral<R>::value &&
+        !std::is_same<T, bool>::value && !std::is_same<R, bool>::value,
+    R>::type
+safe_cast_int(T value) {
+    using wider_unsigned_t =
+        typename std::conditional<sizeof(T) >= sizeof(R), T,
+                                  typename std::make_unsigned<R>::type>::type;
+    if (static_cast<wider_unsigned_t>(value) >
+        static_cast<wider_unsigned_t>(std::numeric_limits<R>::max())) {
         throw std::invalid_argument{"value > result max"};
     }
     return static_cast<R>(value);

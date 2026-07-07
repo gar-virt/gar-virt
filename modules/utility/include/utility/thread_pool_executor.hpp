335fb31290ce8f5e411bf0c86c940048861d8fc2
Steffen André Langnes <steffenl.sw@gmail.com>
2026-07-07 16:49:27 +0900

    Add thread pool executor scaling
    ThreadPoolExecutor now takes a thread count range instead of a fixed
    count, allowing the threads to grow/shrink based on the work backlog.

--- modules/utility/include/utility/thread_pool_executor.hpp
+++ modules/utility/include/utility/thread_pool_executor.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <utility/uuid.hpp>
+
 #include <condition_variable>
 #include <functional>
 #include <mutex>
 #include <optional>
 #include <queue>
+#include <string>
 #include <thread>
+#include <unordered_map>
 #include <vector>
 
 namespace ls_gitea_runner::utility {
@@ -14,25 +18,40 @@ class ThreadPoolExecutor final {
     using ULock = std::unique_lock<std::mutex>;
     using Lock = std::lock_guard<std::mutex>;
 
+    struct Worker {
+        std::string id;
+        std::jthread thread;
+    };
+
 public:
     using TaskFn = std::move_only_function<void()>;
 
-    ThreadPoolExecutor(std::optional<size_t> thread_count = std::nullopt) {
-        add_workers(thread_count.value_or(std::thread::hardware_concurrency()));
+    ThreadPoolExecutor(std::optional<size_t> min_thread_count = std::nullopt,
+                       std::optional<size_t> max_thread_count = std::nullopt)
+            : m_min_thread_count{min_thread_count.value_or(std::thread::hardware_concurrency())},
+              m_max_thread_count{max_thread_count.value_or(m_min_thread_count)} {
+        add_fixed_workers(m_min_thread_count);
     }
 
-    ~ThreadPoolExecutor() {
+    ~ThreadPoolExecutor() { stop(); }
+
+    void stop() noexcept {
+        std::vector<std::jthread> threads;
         {
             Lock lock{m_mutex};
             m_stop = true;
+            threads.reserve(m_workers.size());
+            for (auto& [id, worker] : m_workers) {
+                threads.push_back(std::move(worker.thread));
+            }
         }
         m_cv.notify_all();
-        for (auto& thread : m_workers) {
+        for (auto& thread : threads) {
             thread.join();
         }
     }
 
-    void cancel() {
+    void cancel() noexcept {
         {
             Lock lock{m_mutex};
             m_cancel = true;
@@ -49,56 +68,84 @@ public:
             m_queue.push([work = std::forward<F>(work), ... args = std::forward<Args>(args)]() mutable {
                 std::invoke(std::move(work), std::move(args)...);
             });
+            if (m_busy_count >= m_workers.size() && m_workers.size() < m_max_thread_count) {
+                add_worker(true);
+            }
         }
         m_cv.notify_one();
     }
 
-    size_t get_thread_count() const {
-        Lock lock{m_mutex};
-        return m_workers.size();
-    }
-
 private:
-    void worker_fn() {
+    void worker_fn(std::string worker_id, bool is_ondemand) {
         while (true) {
-            TaskFn task;
-            {
-                ULock lock{m_mutex};
-                m_cv.wait(lock, [this] { return m_cancel || m_stop || !m_queue.empty(); });
-                if (m_cancel || (m_stop && m_queue.empty())) {
-                    return;
-                }
-                if (m_queue.empty()) {
+            ULock lock{m_mutex};
+        dead_workers_loop:
+            for (size_t i{m_dead_workers.size()}; i > 0; --i) {
+                auto& dead_worker_ref{m_dead_workers.at(i - 1)};
+                // This should never happen because dying worker threads exit without coming back here.
+                // The check is only here to make it clear that we don't want a deadlock due to a self-join.
+                if (dead_worker_ref.id == worker_id) {
                     continue;
                 }
-                task = std::move(m_queue.front());
-                m_queue.pop();
+                auto dead_worker{std::move(dead_worker_ref)};
+                m_dead_workers.erase(std::next(m_dead_workers.begin(), i - 1));
+                lock.unlock();
+                dead_worker.thread.join();
+                lock.lock();
+                goto dead_workers_loop;
             }
+            m_cv.wait(lock, [this] { return m_cancel || m_stop || !m_queue.empty(); });
+            if (m_cancel || (m_stop && m_queue.empty())) {
+                return;
+            }
+            if (m_queue.empty()) {
+                continue;
+            }
+            auto task{std::move(m_queue.front())};
+            m_queue.pop();
+            ++m_busy_count;
+            lock.unlock();
             try {
                 task();
             } catch (...) {
                 // Ignore
             }
+            lock.lock();
+            --m_busy_count;
+            if (is_ondemand && m_busy_count < m_workers.size() && m_queue.empty()) {
+                auto worker{std::move(m_workers.at(worker_id))};
+                m_workers.erase(worker_id);
+                m_dead_workers.emplace_back(std::move(worker));
+                return;
+            }
         }
     }
 
-    void add_workers(size_t count) {
+    void add_fixed_workers(size_t count) {
         Lock lock{m_mutex};
         while (m_workers.size() < count) {
-            add_worker();
+            add_worker(false);
         }
     }
 
-    void add_worker() {
-        m_workers.emplace_back([this] { worker_fn(); });
+    void add_worker(bool is_ondemand) {
+        auto worker_id{uuid()};
+        m_workers.emplace(worker_id, Worker{
+                                         .id = worker_id,
+                                         .thread = std::jthread{[=, this] { worker_fn(worker_id, is_ondemand); }},
+                                     });
     }
 
+    size_t m_min_thread_count{};
+    size_t m_max_thread_count{};
+    size_t m_busy_count{};
     mutable std::mutex m_mutex;
     std::condition_variable m_cv;
     bool m_stop{};
     bool m_cancel{};
     std::queue<TaskFn> m_queue;
-    std::vector<std::jthread> m_workers;
+    std::unordered_map<std::string, Worker> m_workers;
+    std::vector<Worker> m_dead_workers;
 };
 
 } // namespace ls_gitea_runner::utility

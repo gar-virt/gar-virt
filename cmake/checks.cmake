770820e0080d8e1f4f42f8b9d0fd5e0240c565c3
Steffen André Langnes <steffenl.sw@gmail.com>
2026-07-18 10:55:04 +0900

    Turn warnings into errors

--- cmake/checks.cmake
+++ cmake/checks.cmake
@@ -2,6 +2,7 @@ add_library(checks INTERFACE)
 
 if((CMAKE_C_COMPILER_ID MATCHES "((^GNU)|Clang)$") OR (CMAKE_CXX_COMPILER_ID MATCHES "((^GNU)|Clang)$"))
     target_compile_options(checks INTERFACE
+        -Werror
         -Wall
         -Wextra
         -Wpedantic

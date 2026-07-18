374efd9ee1e71ad0c6de4bcfe773bbc4ee3772a8
Steffen André Langnes <steffenl.sw@gmail.com>
2026-07-18 09:25:05 +0900

    Clean up ASan/TSan flags

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -3,15 +3,24 @@ if(CMAKE_CONFIGURATION_TYPES)
     list(APPEND CMAKE_CONFIGURATION_TYPES "ASan" "TSan")
 endif()
 
-if((CMAKE_C_COMPILER_ID MATCHES "((^GNU)|Clang)$") OR (CMAKE_CXX_COMPILER_ID MATCHES "((^GNU)|Clang)$"))
-    # Address + undefined behavior sanitizer
-    set(CMAKE_C_FLAGS_ASAN "-g -O1 -fsanitize=address,undefined -fno-sanitize-recover=all -fno-omit-frame-pointer -fno-optimize-sibling-calls")
-    set(CMAKE_CXX_FLAGS_ASAN "-g -O1 -fsanitize=address,undefined -fno-sanitize-recover=all -fno-omit-frame-pointer -fno-optimize-sibling-calls")
-    set(CMAKE_EXE_LINKER_FLAGS_ASAN "-fsanitize=address,undefined")
-    set(CMAKE_SHARED_LINKER_FLAGS_ASAN "-fsanitize=address,undefined")
-    # Thread sanitizer - cannot be combined with -fsanitize=address
-    set(CMAKE_C_FLAGS_TSAN "-g -O1 -fsanitize=thread")
-    set(CMAKE_CXX_FLAGS_TSAN "-g -O1 -fsanitize=thread")
-    set(CMAKE_EXE_LINKER_FLAGS_TSAN "-fsanitize=thread")
-    set(CMAKE_SHARED_LINKER_FLAGS_TSAN "-fsanitize=thread")
-endif()
+block()
+    if((CMAKE_C_COMPILER_ID MATCHES "((^GNU)|Clang)$") OR (CMAKE_CXX_COMPILER_ID MATCHES "((^GNU)|Clang)$"))
+        # Address + undefined behavior sanitizer
+        set(COMPILER_FLAGS_ASAN "-g -O1 -fsanitize=address,undefined -fno-sanitize-recover=all -fno-omit-frame-pointer -fno-optimize-sibling-calls")
+        set(LINKER_FLAGS_ASAN "-fsanitize=address,undefined")
+
+        set(CMAKE_C_FLAGS_ASAN "${COMPILER_FLAGS_ASAN}" PARENT_SCOPE)
+        set(CMAKE_CXX_FLAGS_ASAN "${COMPILER_FLAGS_ASAN}" PARENT_SCOPE)
+        set(CMAKE_EXE_LINKER_FLAGS_ASAN "${LINKER_FLAGS_ASAN}" PARENT_SCOPE)
+        set(CMAKE_SHARED_LINKER_FLAGS_ASAN "${LINKER_FLAGS_ASAN}" PARENT_SCOPE)
+
+        # Thread sanitizer - cannot be combined with -fsanitize=address
+        set(COMPILER_FLAGS_TSAN "-g -O1 -fsanitize=thread")
+        set(LINKER_FLAGS_TSAN "-fsanitize=thread")
+
+        set(CMAKE_C_FLAGS_TSAN "${COMPILER_FLAGS_TSAN}" PARENT_SCOPE)
+        set(CMAKE_CXX_FLAGS_TSAN "${COMPILER_FLAGS_TSAN}" PARENT_SCOPE)
+        set(CMAKE_EXE_LINKER_FLAGS_TSAN "${LINKER_FLAGS_TSAN}" PARENT_SCOPE)
+        set(CMAKE_SHARED_LINKER_FLAGS_TSAN "${LINKER_FLAGS_TSAN}" PARENT_SCOPE)
+    endif()
+endblock()

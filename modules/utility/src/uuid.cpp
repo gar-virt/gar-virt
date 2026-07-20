0b9c94dd6200294abef8019b97d24637c6c925ec
Steffen André Langnes <steffenl.sw@gmail.com>
2026-07-20 17:25:02 +0900

    uuid() refactor and fix
    Copies the result once on Windows instead of twice, and eliminates the
    copy on other platforms.
    
    Although an error should never occur, the result of StringCchCopyA() is
    now checked for errors instead of returning a string containing null
    characters.

--- modules/utility/src/uuid.cpp
+++ modules/utility/src/uuid.cpp
@@ -1,10 +1,10 @@
 #include <utility/uuid.hpp>
 
-#include <stdexcept>
-
 #ifdef _WIN32
     #include <rpc.h>
     #include <strsafe.h>
+
+    #include <stdexcept>
 #else
     #include <uuid/uuid.h>
 #endif
@@ -15,39 +15,39 @@ constexpr int uuid_length{36};
 }
 
 std::string uuid() {
-    // 36-byte string + terminator
-    char uuid_cstr[uuid_length + 1]{};
+    std::string uuid_str(uuid_length, '\0');
 
 #ifdef _WIN32
     UUID uuid{};
-    RPC_STATUS status{};
-    RPC_CSTR uuid_str{};
+    bool ok{};
+    RPC_CSTR uuid_ucstr{};
 
     do {
-        if ((status = ::UuidCreate(&uuid)) != RPC_S_OK) {
+        if (ok = ::UuidCreate(&uuid) == RPC_S_OK; !ok) {
             break;
         }
 
-        if ((status = ::UuidToStringA(&uuid, &uuid_str)) != RPC_S_OK) {
+        if (ok = ::UuidToStringA(&uuid, &uuid_ucstr) == RPC_S_OK; !ok) {
             break;
         }
     } while (false);
 
-    if (uuid_str) {
-        ::StringCchCopyA(uuid_cstr, _countof(uuid_cstr), reinterpret_cast<const char*>(uuid_str));
-        ::RpcStringFreeA(&uuid_str);
+    if (uuid_ucstr) {
+        ok = SUCCEEDED(
+            ::StringCchCopyA(uuid_str.data(), uuid_str.size() + 1, reinterpret_cast<const char*>(uuid_ucstr)));
+        ::RpcStringFreeA(&uuid_ucstr);
     }
 
-    if (status != RPC_S_OK) {
+    if (!ok) {
         throw std::runtime_error{"UUID creation failed"};
     }
 #else
     uuid_t uuid{};
-    ::uuid_generate(uuid);
-    ::uuid_unparse_lower(uuid, uuid_cstr);
+    ::uuid_generate(uuid);                       // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+    ::uuid_unparse_lower(uuid, uuid_str.data()); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 #endif
 
-    return {uuid_cstr, uuid_cstr + uuid_length};
+    return uuid_str;
 }
 
 } // namespace ls_gitea_runner::utility

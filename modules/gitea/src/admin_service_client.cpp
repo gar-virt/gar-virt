a064e92df1035a6383c0cc9f727d33228fb00af3
Steffen André Langnes <steffenl.sw@gmail.com>
2026-07-25 10:21:04 +0900

    wip

--- modules/gitea/src/admin_service_client.cpp
+++ modules/gitea/src/admin_service_client.cpp
@@ -1,55 +1,19 @@
-module;
-
-#include <boost/json.hpp>
-
 export module gitea:admin;
 
 import utility.http;
 import utility.misc;
 
 import std;
+import std.compat;
 
-export namespace ls_gitea_runner::gitea {
+namespace ls_gitea_runner::gitea {
 
-class AdminServiceClient final {
+export class AdminServiceClient final {
 public:
-    AdminServiceClient(const std::string& instance_url, std::string token)
-            : m_token{std::move(token)}, m_client{utility::http_path_join(instance_url, "/api/v1/admin")} {
-        m_client.add_request_middleware([this](auto& req) {
-            if (req.method == utility::HttpMethod::post) {
-                req.headers.emplace("Content-Type", "application/json");
-            }
-
-            req.headers.emplace("Accept", "application/json");
-
-            if (!m_token.empty()) {
-                req.headers.emplace("Authorization", std::format("token {}", m_token));
-            }
-
-            return true;
-        });
-    }
-
-    ~AdminServiceClient() = default;
-
-    std::expected<std::string, GenericError> get_registration_token() const {
-        auto res{m_client.post("/actions/runners/registration-token", {})};
-        if (!res) {
-            return std::unexpected{res.error()};
-        }
-        try {
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-            const auto* body{reinterpret_cast<const char*>(res->body.data())};
-            const auto j{boost::json::parse(std::string_view{body, res->body.size()}).as_object()};
-            return std::string{j.at("token").as_string()};
-        } catch (const std::exception& ex) {
-            return std::unexpected{GenericError{std::format("Failed to parse registration token: {}", ex.what())}};
-        }
-    }
-
-    std::expected<void, GenericError> remove_runner(uint64_t runner_id) const {
-        return m_client.del(std::format("/actions/runners/{}", runner_id)).transform([](const auto&) {});
-    }
+    AdminServiceClient(const std::string& instance_url, std::string token);
+    ~AdminServiceClient();
+    std::expected<std::string, GenericError> get_registration_token() const;
+    std::expected<void, GenericError> remove_runner(uint64_t runner_id) const;
 
 private:
     std::string m_token;
---
+++ modules/gitea/src/admin_service_client_impl.cpp
@@ -0,0 +1,54 @@
+module;
+
+#include <boost/json.hpp>
+
+module gitea:admin_impl;
+
+import :admin;
+
+import utility.http;
+import utility.misc;
+
+import std;
+
+namespace ls_gitea_runner::gitea {
+
+AdminServiceClient::AdminServiceClient(const std::string& instance_url, std::string token)
+        : m_token{std::move(token)}, m_client{utility::http_path_join(instance_url, "/api/v1/admin")} {
+    m_client.add_request_middleware([this](auto& req) {
+        if (req.method == utility::HttpMethod::post) {
+            req.headers.emplace("Content-Type", "application/json");
+        }
+
+        req.headers.emplace("Accept", "application/json");
+
+        if (!m_token.empty()) {
+            req.headers.emplace("Authorization", std::format("token {}", m_token));
+        }
+
+        return true;
+    });
+}
+
+AdminServiceClient::~AdminServiceClient() = default;
+
+std::expected<std::string, GenericError> AdminServiceClient::get_registration_token() const {
+    auto res{m_client.post("/actions/runners/registration-token", {})};
+    if (!res) {
+        return std::unexpected{res.error()};
+    }
+    try {
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+        const auto* body{reinterpret_cast<const char*>(res->body.data())};
+        const auto j{boost::json::parse(std::string_view{body, res->body.size()}).as_object()};
+        return std::string{j.at("token").as_string()};
+    } catch (const std::exception& ex) {
+        return std::unexpected{GenericError{std::format("Failed to parse registration token: {}", ex.what())}};
+    }
+}
+
+std::expected<void, GenericError> AdminServiceClient::remove_runner(uint64_t runner_id) const {
+    return m_client.del(std::format("/actions/runners/{}", runner_id)).transform([](const auto&) {});
+}
+
+} // namespace ls_gitea_runner::gitea

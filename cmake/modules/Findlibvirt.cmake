22fd497a15b0b08579c7416d2740456b42ec4571
Steffen André Langnes <steffenl.sw@gmail.com>
2026-07-31 10:03:21 +0900

    Add target for libvirt headers and separate qemu include

--- cmake/modules/Findlibvirt.cmake
+++ cmake/modules/Findlibvirt.cmake
@@ -1,6 +1,7 @@
 include(FindPackageHandleStandardArgs)
 
 find_path(libvirt_INCLUDE_DIR NAMES libvirt/libvirt.h)
+find_path(libvirt_qemu_INCLUDE_DIR NAMES libvirt/libvirt-qemu.h)
 find_library(libvirt_LIBRARY NAMES virt)
 find_library(libvirt_qemu_LIBRARY NAMES virt-qemu)
 
@@ -17,18 +18,26 @@ find_package_handle_standard_args(
 mark_as_advanced(libvirt_INCLUDE_DIR libvirt_LIBRARY libvirt_qemu_LIBRARY libvirt_LIBRARIES)
 
 if(libvirt_FOUND)
+    if (NOT TARGET libvirt::virt_headers)
+        add_library(libvirt::virt_headers INTERFACE IMPORTED)
+        target_include_directories(libvirt::virt_headers SYSTEM INTERFACE ${libvirt_INCLUDE_DIR})
+    endif()
     if (NOT TARGET libvirt::virt)
         add_library(libvirt::virt SHARED IMPORTED)
-        target_include_directories(libvirt::virt SYSTEM INTERFACE ${libvirt_INCLUDE_DIR})
+        target_link_libraries(libvirt::virt INTERFACE libvirt::virt_headers)
         set_target_properties(
             libvirt::virt
             PROPERTIES
                 IMPORTED_LOCATION ${libvirt_LIBRARY}
         )
     endif()
+    if(NOT TARGET libvirt::qemu_headers)
+        add_library(libvirt::qemu_headers INTERFACE IMPORTED)
+        target_include_directories(libvirt::qemu_headers SYSTEM INTERFACE ${libvirt_qemu_INCLUDE_DIR})
+    endif()
     if(NOT TARGET libvirt::qemu)
         add_library(libvirt::qemu SHARED IMPORTED)
-        target_include_directories(libvirt::qemu SYSTEM INTERFACE ${libvirt_INCLUDE_DIR})
+        target_link_libraries(libvirt::qemu INTERFACE libvirt::qemu_headers)
         set_target_properties(
             libvirt::qemu
             PROPERTIES

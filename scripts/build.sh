783cb416fa88e3b215f0ca9c1f1c74dbfd8dc9e3
Steffen André Langnes <steffenl.sw@gmail.com>
2026-08-09 11:21:14 +0900

    Add initial deb package cpack config

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,4 @@ deps/fetch.sh --build-dir build
 cmake -G Ninja -B build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 cpack -G TXZ -B build/dist --config build/CPackConfig.cmake -DCPACK_COMPONENTS_ALL='app_archive'
+cpack -G DEB -B build/dist --config build/CPackConfig.cmake -DCPACK_COMPONENTS_ALL='app_package'

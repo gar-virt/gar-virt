e28b3decab2a51d3b9772c7224caa6bcaf04b8a8
Steffen André Langnes <steffenl.sw@gmail.com>
2026-08-10 23:58:31 +0900

    Add CMake option for toggling static linking

--- cmake/static_linking.cmake
+++ cmake/static_linking.cmake
@@ -1,7 +1,10 @@
 include_guard()
+include("${CMAKE_CURRENT_LIST_DIR}/options.cmake")
 
-set(OPENSSL_USE_STATIC_LIBS ON)
-set(ZLIB_USE_STATIC_LIBS ON)
+if(GARVIRT_ENABLE_STATIC_LINKING)
+    set(OPENSSL_USE_STATIC_LIBS ON)
+    set(ZLIB_USE_STATIC_LIBS ON)
 
-add_compile_options(-static-libgcc -static-libstdc++)
-add_link_options(-static-libgcc -static-libstdc++)
+    add_compile_options(-static-libgcc -static-libstdc++)
+    add_link_options(-static-libgcc -static-libstdc++)
+endif()

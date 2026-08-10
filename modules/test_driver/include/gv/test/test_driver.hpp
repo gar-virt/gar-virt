c4e23749e4df2d17d0cd8c18bcc052ad2adbe9a1
Steffen André Langnes <steffenl.sw@gmail.com>
2026-08-10 23:59:21 +0900

    Add test driver

---
+++ modules/test_driver/include/gv/test/test_driver.hpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2024 Steffen André Langnes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <exception>
+#include <functional>
+#include <map>
+#include <string>
+
+namespace gv::test {
+
+struct failure_info {
+    const char* condition;
+    const char* file;
+    int line;
+};
+
+struct test_failure : public std::exception {
+    explicit test_failure(const failure_info& info) noexcept : m_info{info} {}
+    const char* what() const noexcept override { return "test failure"; }
+    const failure_info& info() const noexcept { return m_info; }
+
+private:
+    failure_info m_info;
+};
+
+class test_reg {
+public:
+    test_reg() = default;
+
+    test_reg(const char* name, std::function<void()> fn) noexcept : m_name{name}, m_fn{std::move(fn)} {}
+
+    const std::string& name() const noexcept { return m_name; }
+    void invoke() const { m_fn(); }
+
+private:
+    std::string m_name;
+    std::function<void()> m_fn;
+};
+
+struct auto_test_reg {
+    explicit auto_test_reg(test_reg reg) noexcept { tests()[reg.name()] = std::move(reg); }
+
+    static std::map<std::string, test_reg>& tests();
+};
+
+// NOLINTBEGIN(cppcoreguidelines-macro-usage, misc-use-anonymous-namespace)
+
+#define MAKE_TEST_CASE_NAME2(name, counter) name##counter
+#define MAKE_TEST_CASE_NAME(name, counter) MAKE_TEST_CASE_NAME2(name, counter)
+
+#define TEST_CASE_INTERNAL(name, counter)                                                                              \
+    static void MAKE_TEST_CASE_NAME(garvirt_test_driver_case_, counter)();                                             \
+    namespace {                                                                                                        \
+    const ::gv::test::auto_test_reg MAKE_TEST_CASE_NAME(garvirt_test_driver_case_reg_, counter){                       \
+        {name, MAKE_TEST_CASE_NAME(garvirt_test_driver_case_, counter)}};                                              \
+    }                                                                                                                  \
+    static void MAKE_TEST_CASE_NAME(garvirt_test_driver_case_, counter)()
+
+#define TEST_CASE(name) TEST_CASE_INTERNAL(name, __LINE__)
+
+#define REQUIRE(...)                                                                                                   \
+    if (!static_cast<bool>(__VA_ARGS__)) {                                                                             \
+        throw ::gv::test::test_failure{::gv::test::failure_info{#__VA_ARGS__, __FILE__, __LINE__}};                    \
+    }
+
+#define REQUIRE_FALSE(...)                                                                                             \
+    if (static_cast<bool>(__VA_ARGS__)) {                                                                              \
+        throw ::gv::test::test_failure{::gv::test::failure_info{#__VA_ARGS__, __FILE__, __LINE__}};                    \
+    }
+
+#define REQUIRE_THROW(exception, ...)                                                                                  \
+    {                                                                                                                  \
+        bool did_throw{};                                                                                              \
+        try {                                                                                                          \
+            (__VA_ARGS__)();                                                                                           \
+        } catch (const exception&) {                                                                                   \
+            did_throw = true;                                                                                          \
+        }                                                                                                              \
+        REQUIRE(did_throw);                                                                                            \
+    }
+
+#define REQUIRE_NOTHROW(...)                                                                                           \
+    {                                                                                                                  \
+        bool did_throw{};                                                                                              \
+        try {                                                                                                          \
+            (__VA_ARGS__)();                                                                                           \
+        } catch (...) {                                                                                                \
+            did_throw = true;                                                                                          \
+        }                                                                                                              \
+        REQUIRE(!did_throw);                                                                                           \
+    }
+
+#define SECTION(name)
+
+// NOLINTEND(cppcoreguidelines-macro-usage)
+
+} // namespace gv::test
---
+++ modules/test_driver/src/test_driver.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2024 Steffen André Langnes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gv/test/test_driver.hpp>
+
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+namespace gv::test {
+
+std::map<std::string, test_reg>& auto_test_reg::tests() {
+    static std::map<std::string, test_reg> instance;
+    return instance;
+}
+
+namespace {
+
+struct failure_exit_codes {
+    enum type { success = 0, failure = 1, exception_thrown = 2, no_tests_found = 3, test_not_found = 4 };
+};
+
+int cmd_help() {
+    std::cout << "Usage: program [--[help|list]|test_name].\n";
+    return 0;
+}
+
+int cmd_list() {
+    auto& tests{auto_test_reg::tests()};
+    if (tests.empty()) {
+        std::cerr << "No tests found.\n";
+        return failure_exit_codes::no_tests_found;
+    }
+    for (const auto& test : tests) {
+        std::cout << test.second.name() << '\n';
+    }
+    return failure_exit_codes::success;
+}
+
+int cmd_run_test(const std::string& test_name) {
+    auto& tests{auto_test_reg::tests()};
+    auto found{tests.find(test_name)};
+    if (found == tests.end()) {
+        std::cerr << "Test not found: " << test_name << '\n';
+        return failure_exit_codes::test_not_found;
+    }
+    auto exit_code{failure_exit_codes::failure};
+    const auto& test{found->second};
+    try {
+        test.invoke();
+        exit_code = failure_exit_codes::success;
+    } catch (const test_failure& e) {
+        const auto& info{e.info()};
+        std::cerr << "Test \"" << test.name() << "\" failed [" << info.condition << "] at " << info.file << ":"
+                  << info.line << ".\n";
+        exit_code = failure_exit_codes::failure;
+    } catch (const std::exception& e) {
+        std::cerr << "Test \"" << test.name() << "\" threw exception with message \"" << e.what() << "\".\n";
+        exit_code = failure_exit_codes::exception_thrown;
+    } catch (...) {
+        std::cerr << "Test \"" << test.name() << "\" threw unknown exception.\n";
+        exit_code = failure_exit_codes::exception_thrown;
+    }
+    return exit_code;
+}
+
+int cmd_run_all_tests() {
+    auto& tests{auto_test_reg::tests()};
+    if (tests.empty()) {
+        return failure_exit_codes::no_tests_found;
+    }
+    size_t name_width{};
+    for (const auto& it : tests) {
+        const auto& test{it.second};
+        name_width = std::max(name_width, test.name().size());
+    }
+    size_t success_count{};
+    for (const auto& it : tests) {
+        const auto& test{it.second};
+        std::cout << test.name() << std::setfill('.') << std::setw(static_cast<int>(name_width - test.name().size()))
+                  << "" << std::setw(0) << ": ";
+        try {
+            test.invoke();
+            ++success_count;
+            std::cout << "OK\n";
+        } catch (const test_failure& e) {
+            const auto& info{e.info()};
+            std::cout << "FAIL\n";
+            std::cerr << "Test \"" << test.name() << "\" failed [" << info.condition << "] at " << info.file << ":"
+                      << info.line << ".\n";
+        } catch (const std::exception& e) {
+            std::cout << "FAIL\n";
+            std::cerr << "Test \"" << test.name() << "\" threw exception with message \"" << e.what() << "\".\n";
+        } catch (...) {
+            std::cout << "FAIL\n";
+            std::cerr << "Test \"" << test.name() << "\" threw unknown exception.\n";
+        }
+    }
+    if (success_count == tests.size()) {
+        return failure_exit_codes::success;
+    }
+    return failure_exit_codes::failure;
+}
+
+} // namespace
+} // namespace gv::test
+
+int main(int argc, const char* argv[]) {
+    using namespace gv::test;
+    try {
+        const std::deque<std::string> args{argv, argv + argc};
+        if (args.size() > 1) {
+            const std::string& arg{args.at(1)};
+            if (arg == "--help") {
+                return cmd_help();
+            }
+            if (arg == "--list") {
+                return cmd_list();
+            }
+            return cmd_run_test(arg);
+        }
+        return cmd_run_all_tests();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return failure_exit_codes::exception_thrown;
+    } catch (...) {
+        std::cerr << "Unknown error.\n";
+        return failure_exit_codes::exception_thrown;
+    }
+}
